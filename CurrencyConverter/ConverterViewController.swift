989f6c111806055a36da1eceb0366f576b3ebc63
Brandon <brandon@Brandons-MacBook-Pro.local>
2017-02-24 16:30:56 -0800

    2/24

--- CurrencyConverter/ConverterViewController.swift
+++ CurrencyConverter/ConverterViewController.swift
@@ -13,10 +13,14 @@ class ConverterViewController : UIViewController, UITextFieldDelegate {
     
     //MARK: -Variables/connections
     var cr = CurrencyRate(CurrencyOne: "error", CurrencyTwo: "error", CountryOne: "error", CountryTwo: "error", Rate: 0)
+    let currencyLogoDic : [String:String] = ["BGN":"лв", "BRL":"R$", "CHF":"chf", "CNY":"¥", "CZK":"Kč", "DKK":"kr", "EUR":"€", "GBP":"£", "HRK":"kn", "HUF":"Ft", "IDR":"Rp", "ILS":"₪", "INR":"₹", "JPY":"¥", "KRW":"₩", "MYR":"RM", "NOK":"kr", "PHP":"₱", "PLN":"zł", "RON":"lei", "RUB":"₽", "SEK":"kr", "THB":"฿", "TRY":"₤", "ZAR":"R"]
+    
+    var currencyLogoOne = "$"
+    var currencyLogoTwo = "$"
     
     var calculatedValue:Double = 0 {
         didSet {
-            currencyTwoTF.text = String(calculatedValue)
+            currencyTwoTF.text =  currencyLogoTwo + String(calculatedValue)
         }
     }
     
@@ -35,11 +39,17 @@ class ConverterViewController : UIViewController, UITextFieldDelegate {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        if let logoOne = currencyLogoDic[cr.currencyOne]{
+            currencyLogoOne = logoOne
+        }
+        if let logoTwo = currencyLogoDic[cr.currencyTwo]{
+            currencyLogoTwo = logoTwo
+        }
         currencyOneTF.becomeFirstResponder()
-        currencyOneLabel.text =   cr.currencyOne
+        currencyOneLabel.text =   currencyLogoOne + cr.currencyOne
         currencyTwoLabel.text =   cr.currencyTwo
         currencyOneTF.text =      String(1.00)
-        currencyTwoTF.text =      String(cr.rate * 1.00)
+        currencyTwoTF.text =      currencyLogoTwo + String(cr.rate * 1.00)
         let flgImageOne =         cr.currencyOne
         flagOne.image =           UIImage(named: flgImageOne)
         let flgImageTwo =         cr.currencyTwo
@@ -57,6 +67,9 @@ class ConverterViewController : UIViewController, UITextFieldDelegate {
         if (!(currencyOneTF.text?.isEmpty)!){
             let value = Double(currencyOneTF.text!)
             calculatedValue = (cr.rate * value!)
+        }
+        else {
+            currencyTwoTF.text = currencyLogoTwo
         }
     }
 }
